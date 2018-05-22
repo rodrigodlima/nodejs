e6c30655001017b5833936e4afdf1b13c0ccad16
Rodrigo Lima <rodrigodlima@gmail.com>
2018-05-22 12:25:01 -0300

    test with Jenkins and Mocha

---
+++ tests/myapp.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+oiaiosass
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world! Jenkins test').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
