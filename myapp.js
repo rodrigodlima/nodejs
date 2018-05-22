9e6b087f95d8a8c6a471219ba27a7c50273e8e86
Rodrigo Lima <rodrigodlima@gmail.com>
2018-05-22 11:27:16 -0300

    Testing Jenkins Job

--- myapp.js
+++ myapp.js
@@ -5,12 +5,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
+  res.status(200).send('Hello, world! Jenkins test').end();
 });
 
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
 });
