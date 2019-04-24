c89947aedd38ae8dd6a0d76ece28b7482f444f45
Rodrigo Lima <rodrigodlima@gmail.com>
2019-04-23 22:57:07 -0300

    Adicionado log winston

--- myapp.js
+++ myapp.js
@@ -1,11 +1,26 @@
 #!/usr/bin/env node
 
 const express = require('express');
-
 const app = express();
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log` 
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello, world! Jenkins test again').end();
+  logger.info('http access code 200 OK');
 });
 
 // Start the server
